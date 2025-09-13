bdb2e8350000475279fbba79590b07fb9730e0cd
lethanh23456 <thanhld2006.ptit@gmail.com>
2025-09-13 21:37:22 +0700

    sắp xếp chọn

--- contest/sapxepchon.cpp
+++ contest/sapxepchon.cpp
@@ -1,1 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
 
+
+int main() {
+    ll n; cin >> n;
+    vector<ll> a(n + 1);
+    for (ll i = 1; i <= n; i++) {
+        cin >> a[i];
+    }
+
+    for (ll i = 1; i < n; i++) {
+        auto it = min_element(a.begin() + i, a.begin() + n + 1);
+        swap(a[i], *it);
+
+        cout << "Buoc " << i << ": ";
+        for (ll j = 1; j <= n; j++) cout << a[j] << " ";
+        cout << "\n";
+    }
+}
